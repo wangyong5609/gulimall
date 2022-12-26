d53a27f4c810604cb22b864a4033fe0b1d419c72
wangyong5609 <1137917270@qq.com>
2022-12-26 20:40:02 +0800

    P21：分布式组件-SpringCloud Alibaba-Nacos注册中心

--- coupon/src/main/java/com/hhh/mall/coupon/CouponApplication.java
+++ coupon/src/main/java/com/hhh/mall/coupon/CouponApplication.java
@@ -3,8 +3,11 @@ package com.hhh.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @MapperScan("com.hhh.mall.coupon.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CouponApplication {
 
     public static void main(String[] args) {
--- ware/src/main/java/com/hhh/mall/ware/WareApplication.java
+++ ware/src/main/java/com/hhh/mall/ware/WareApplication.java
@@ -3,8 +3,11 @@ package com.hhh.mall.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @MapperScan("com.hhh.mall.ware.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WareApplication {
 
     public static void main(String[] args) {
