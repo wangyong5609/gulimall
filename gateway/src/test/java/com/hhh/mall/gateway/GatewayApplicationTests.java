340cd85ff19e95b693e8fa9230d9032c78aa7e90
wangyong5609 <1137917270@qq.com>
2022-12-28 21:11:01 +0800

    27、分布式组件-SpringCloud-Gateway-创建&测试API网关

---
+++ gateway/src/test/java/com/hhh/mall/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhh.mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
