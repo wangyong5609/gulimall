69fe5ca628422a5721c35d09075f259bcbdec49f
wangyong5609 <1137917270@qq.com>
2022-12-30 22:13:21 +0800

    45、商品服务-API-三级分类-查询-递归树形结构数据获取

--- product/src/main/java/com/hhh/mall/product/service/impl/CategoryServiceImpl.java
+++ product/src/main/java/com/hhh/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.hhh.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +30,37 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 查询所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // 组装父子结构
+        // 1. 找到所有1级分类
+        List<CategoryEntity> level1 = entities.stream().filter(entity -> entity.getParentCid() == 0)
+                .map(menu -> {
+                    menu.setChildren(getChildren(menu, entities));
+                    return menu;
+                })
+                .sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0: menu1.getSort())  - (menu2.getSort() == null ? 0: menu2.getSort());
+                })
+                .collect(Collectors.toList());
+
+
+        return level1;
+    }
+
+    // 查询子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        return all.stream().filter(menu -> menu.getParentCid() == root.getCatId())
+                .map(entity -> {
+                    entity.setChildren(getChildren(entity, all));
+                    return entity;
+                })
+                .sorted((menu1, menu2) -> {
+                    return (menu1.getSort() == null ? 0: menu1.getSort())  - (menu2.getSort() == null ? 0: menu2.getSort());
+                })
+                .collect(Collectors.toList());
+    }
 }
